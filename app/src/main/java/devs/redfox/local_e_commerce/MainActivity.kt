f85d707455ff4d97169dac0ea72f46a9163ce90e
Madhur Mehta <12madhurmehta@gmail.com>
2022-07-09 12:29:44 +0530

    Initial Commit

---
+++ app/src/main/java/devs/redfox/local_e_commerce/MainActivity.kt
@@ -0,0 +1,11 @@
+package devs.redfox.local_e_commerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
