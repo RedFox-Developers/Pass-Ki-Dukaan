6af6a8e5a6be7099041b0e9c8efffa6dd8e79311
Debayan Ghosh Dastider <debz.exe@gmail.com>
2022-07-31 20:56:33 +0530

    Added Total cost, total item in cart feature. Added delete item from cart feature.

--- app/src/main/java/devs/redfox/local_e_commerce/adapter/CartAdapter.kt
+++ app/src/main/java/devs/redfox/local_e_commerce/adapter/CartAdapter.kt
@@ -2,12 +2,18 @@ package devs.redfox.local_e_commerce.adapter
 
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import devs.redfox.local_e_commerce.activity.ProductDetailActivity
+import devs.redfox.local_e_commerce.database.AppDatabase
 import devs.redfox.local_e_commerce.database.ProductModel
 import devs.redfox.local_e_commerce.databinding.LayoutCartItemBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class CartAdapter(val context: Context, val list: ArrayList<ProductModel>): RecyclerView.Adapter<CartAdapter.CartViewHolder>() {
 
@@ -24,6 +30,28 @@ class CartAdapter(val context: Context, val list: ArrayList<ProductModel>): Recy
         Glide.with(context).load(list[position].productImage).into(holder.binding.imageView4)
         holder.binding.textView11.text = list[position].productName
         holder.binding.textView12.text = list[position].productSp
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, ProductDetailActivity::class.java)
+            intent.putExtra("id", list[position].productId)
+            context.startActivity(intent)
+        }
+
+
+        val dao = AppDatabase.getInstance(context).productDao()
+        holder.binding.btnDeleteCartItem.setOnClickListener {
+
+            GlobalScope.launch(Dispatchers.IO) {
+                dao.deleteProduct(
+                    ProductModel(
+                        list[position].productId,
+                        list[position].productName,
+                        list[position].productImage,
+                        list[position].productSp
+                    )
+                )
+            }
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/devs/redfox/local_e_commerce/fragment/CartFragment.kt
+++ app/src/main/java/devs/redfox/local_e_commerce/fragment/CartFragment.kt
@@ -1,5 +1,6 @@
 package devs.redfox.local_e_commerce.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import devs.redfox.local_e_commerce.R
+import devs.redfox.local_e_commerce.activity.CategoryActivity
 import devs.redfox.local_e_commerce.adapter.CartAdapter
 import devs.redfox.local_e_commerce.database.AppDatabase
 import devs.redfox.local_e_commerce.database.ProductModel
@@ -36,9 +38,27 @@ class CartFragment : Fragment() {
             binding.rvCartFragment.adapter = CartAdapter(requireContext(),
                 it as ArrayList<ProductModel>
             )
+
+            totalCost(it)
         }
 
         return binding.root
+    }
+
+    private fun totalCost(data: java.util.ArrayList<ProductModel>) {
+        var total = 0
+        for(item in data!!){
+            total += item.productSp!!.toInt()
+        }
+
+        binding.textView13.text = "Total item in cart: ${data.size}"
+        binding.textView14.text = "Total Cost: $total"
+
+        binding.checkout.setOnClickListener {
+            val intent = Intent(context, CategoryActivity::class.java)
+            intent.putExtra("totalCost", total)
+            startActivity(intent)
+        }
     }
 
 }
